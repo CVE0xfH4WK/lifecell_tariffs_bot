f054f33d39b8f11608a35c6650ff59083566b132
Vlad Isakov <hacker_net3055it@outlook.com>
2023-06-16 23:24:07 +0300

    logics beging

---
+++ examples/commit_2/src/question_logics.py
@@ -0,0 +1,15 @@
+class Checker:
+    def __init__(self) -> None:
+        pass
+
+
+    def check_price(self) -> None:
+        pass
+
+
+    def check_net(self) -> None:
+        pass
+
+    
+    def check_signal(self) -> None:
+        pass
